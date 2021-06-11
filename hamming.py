3c86c0878040c186c58bb056a138ccceb3d31be7
robo-monk <rrobomonk@gmail.com>
2021-06-12 02:07:29 +0300

    finish up

--- hamming.py
+++ hamming.py
@@ -2,6 +2,7 @@ import random
 import numpy as np
 from functools import reduce
 from rich import print
+from random import randint
 
 """
 if there's an error, does its position look like ..___1?
@@ -21,6 +22,14 @@ def prepare_block(msg):
     return block
 
 
+def transmit_msg(msg, errors=1):
+    msg = msg.copy()
+    for _ in range(errors):
+        r = randint(0, len(msg)-1)
+        msg[r] = not msg[r]
+
+    return msg
+
 
 def generate_msg(size):
     return np.random.randint(2, size=size) 
@@ -32,4 +41,16 @@ print("message is", msg, "with random parity", calc_parity_bits(msg))
 block = prepare_block(msg)
 print("p block is", block, "with actual parity", calc_parity_bits(block))
 
+print("transmitting msg...")
+tblock = transmit_msg(block)
+print(tblock)
+print(np.array_equal(tblock, block))
+
+parity_bits = calc_parity_bits(tblock)
+print("correcting message at position", parity_bits)
+tblock[parity_bits] = not tblock[parity_bits]
+print(tblock)
+print(np.array_equal(tblock, block))
+
+# TODO implement extended hamming code
 
