3b021f77d8efd822cf783f24ce7a99fb01837c5e
robo-monk <rrobomonk@gmail.com>
2021-06-13 02:27:32 +0300

    hamming distance

--- hamming.py
+++ hamming.py
@@ -10,6 +10,15 @@ if there's an error, does its position look like ..__1_?
 ...
 """
 
+def bit_count(bit_sequence):
+    count = 0
+
+    while bit_sequence:
+        count += (bit_sequence & 1)
+        bit_sequence >>= 1
+
+    return count
+
 def calc_parity_bits(msg):
     return reduce( lambda x, y: x ^ y, [ i for i, bit in enumerate(msg) if bit ] )
 
@@ -31,9 +40,24 @@ def transmit_msg(msg, errors=1):
     return msg
 
 
-def generate_msg(size):
-    return np.random.randint(2, size=size) 
+def generate_msg(size, ran=2):
+    return np.random.randint(ran, size=size) 
 
+def ham_distance(code) -> int:
+    """
+    Calculates the hamming distance of the given code, or returns -1 if it cannot be calculated.
+
+    :param code: The code, consisting out of a list of codewords.
+    :return: The hamming distance of the given code , or -1 if it cannot be calculated.
+    """
+    if len(code) <= 1: return -1
+
+    ww = code[0]
+    dist = min([ bit_count(w^ww) for i, w in enumerate(code[1:]) ])
+    rest = ham_distance(code[1:])
+    if rest != -1 and dist > rest: dist = rest
+
+    return dist
 
 msg = generate_msg(16)
 print("message is", msg, "with random parity", calc_parity_bits(msg))
@@ -52,5 +76,16 @@ tblock[parity_bits] = not tblock[parity_bits]
 print(tblock)
 print(np.array_equal(tblock, block))
 
+
 # TODO implement extended hamming code
+
+msg = [ 0b0000, 0b0011, 0b1100, 0b1111 ] # should be 2
+msg = [ 0b00000000,
+        0b00001111,
+        0b11110000,
+        0b11111111 ] # should be 4
+
+print("testing ham distance code with", msg)
+print("hamming distance is", ham_distance(msg))
+
 
